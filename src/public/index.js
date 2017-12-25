3a8485e7d33b18eadec7fa274f68024a5e63e8d3
Domagoj Katavic <domo.katavic@gmail.com>
2017-12-25 00:16:49 +0000

    print all fields in csv

--- src/public/index.js
+++ src/public/index.js
@@ -9,7 +9,7 @@ app.get('/download', (req, res) => {
     res.set('Content-Type', 'application/csv')
     res.set('Content-Disposition', 'attachment; filename=example.csv')
     res.send(csvFile)
-  })
+  }, { keys: ['profitPerc', 'trades', 'orderBookSymbols'] })
 })
 
 app.get('/download/top100', (req, res) => {
@@ -23,7 +23,7 @@ app.get('/download/top100', (req, res) => {
     res.set('Content-Type', 'application/csv')
     res.set('Content-Disposition', 'attachment; filename=example.csv')
     res.send(csvFile)
-  })
+  }, { keys: ['profitPerc', 'trades', 'orderBookSymbols'] })
 })
 
 app.listen(3000, () => console.log('Server listening on port 3000!'))
